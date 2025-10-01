5b93ec335e15094ee251272d4440acf9e9fe50e8
Nithwin <vmnithwin@gmail.com>
2025-10-01 11:50:51 +0530

    Usecase Component Completed

--- components/Header.jsx
+++ components/Header.jsx
@@ -20,7 +20,7 @@ const Header = ({heroRef}) => {
       end:"+=150",
       scrub:1,
       animation:gsap.to(header, {
-        backgroundColor: 'rgba(0,0,0,0.5)',
+        backgroundColor: 'rgba(0,0,0,0.2)',
         backdropFilter:"blur(16px)",
         ease:"power2.inOut",
       })
@@ -31,7 +31,7 @@ const Header = ({heroRef}) => {
     }
   }, []);
   return (
-    <nav ref={headerRef} id="header" className={`bg-black lg:bg-black/0 w-full p-4 z-50 fixed top-0`}>
+    <nav ref={headerRef} id="header" className={`bg-black/30 lg:bg-transparent w-full p-4 z-50 fixed top-0`}>
       <div className="lg:flex-center lg:justify-between lg:px-[5rem]">
         <div
           className={`flex-center justify-between lg:justify-center lg:gap-10`}
--- components/UseCases.jsx
+++ components/UseCases.jsx
@@ -1,20 +1,129 @@
-import React from 'react'
+import { useCasesListUp } from "@/constants/data";
+import Image from "next/image";
+import React from "react";
 
 const UseCases = () => {
   return (
-    <section className='bg-black min-h-dvh text-white'>
-        <div className=''>
-          <div className='px-2 flex flex-col gap-1'>
-            <p className='text-blue-600 font-semibold text-center text-lg lg:text-xl'>Use Cases</p>
-            <h2 className='text-xl font-bold text-center lg:text-4xl'>Virtual Worlds, Real Solutions</h2>
-            <p className='text-xs lg:text-sm text-center text-white/60 font-medium font-inter'>With over 3 million immersive experiences, Spatial is trusted by global enterprises, top educators, and leading brands to deliver scalable training solutions and create unforgettable marketing moments.</p>
-          </div>
-          <div> 
+    <section className="bg-black min-h-dvh text-white py-[5rem]">
+      <div className="flex flex-col gap-8">
+        <div className="px-2 flex flex-col gap-1">
+          <p className="text-blue-600 font-semibold text-center text-lg lg:text-xl">
+            Use Cases
+          </p>
+          <h2 className="text-xl font-bold text-center lg:text-4xl">
+            Virtual Worlds, Real Solutions
+          </h2>
+          <p className="text-sm lg:text-sm text-center text-white/60 font-medium font-inter">
+            With over 3 million immersive experiences, Spatial is trusted by
+            global enterprises, top educators, and leading brands to deliver
+            scalable training solutions and create unforgettable marketing
+            moments.
+          </p>
+        </div>
 
+        <div className="flex flex-col gap-5">
+          <div className="overflow-hidden scroller">
+            <div className="py-2 usecase_scroller">
+              {useCasesListUp.map((item, index) => (
+                <div
+                  className="group rounded-2xl relative aspect-video w-56 lg:w-96 flex-shrink-0 overflow-hidden cursor-pointer"
+                  key={index}
+                >
+                  <Image
+                    src={"/images/usecases/" + item.img}
+                    alt={item.heading}
+                    width={1000}
+                    height={1000}
+                    className="object-contain mask-b-from-3.5 group-hover:scale-110 transition-transform delay-150 ease-linear"
+                  />
+                  <div className="absolute inset-0 flex flex-col justify-end px-4 pb-2">
+                    <h2 className="text-sm lg:text-lg font-semibold">
+                      {item.heading}
+                    </h2>
+                    <p className="text-xs lg:text-sm text-white/60">
+                      {item.subheading}
+                    </p>
+                  </div>
+                </div>
+              ))}
+
+              {useCasesListUp.map((item, index) => (
+                <div
+                  className="group rounded-2xl relative aspect-video w-56 lg:w-96 flex-shrink-0 overflow-hidden cursor-pointer"
+                  key={item.heading}
+                >
+                  <Image
+                    src={"/images/usecases/" + item.img}
+                    alt={item.heading}
+                    width={1000}
+                    height={1000}
+                    className="object-contain mask-b-from-3.5 group-hover:scale-110 transition-transform delay-150 ease-linear"
+                  />
+                  <div className="absolute inset-0 flex flex-col justify-end px-4 pb-2">
+                    <h2 className="text-sm lg:text-lg font-semibold">
+                      {item.heading}
+                    </h2>
+                    <p className="text-xs lg:text-sm text-white/60">
+                      {item.subheading}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="overflow-hidden scroller">
+            <div className="py-2 usecase_scroller_back">
+              {useCasesListUp.map((item, index) => (
+                <div
+                  className="group rounded-2xl relative aspect-video w-56 lg:w-96 flex-shrink-0 overflow-hidden cursor-pointer"
+                  key={index}
+                >
+                  <Image
+                    src={"/images/usecases/" + item.img}
+                    alt={item.heading}
+                    width={1000}
+                    height={1000}
+                    className="object-contain mask-b-from-3.5 group-hover:scale-110 transition-transform delay-150 ease-linear"
+                  />
+                  <div className="absolute inset-0 flex flex-col justify-end px-4 pb-2">
+                    <h2 className="text-sm lg:text-lg font-semibold">
+                      {item.heading}
+                    </h2>
+                    <p className="text-xs lg:text-sm text-white/60">
+                      {item.subheading}
+                    </p>
+                  </div>
+                </div>
+              ))}
+
+              {useCasesListUp.map((item, index) => (
+                <div
+                  className="group rounded-2xl relative aspect-video w-56 lg:w-96 flex-shrink-0 overflow-hidden cursor-pointer"
+                  key={item.heading}
+                >
+                  <Image
+                    src={"/images/usecases/" + item.img}
+                    alt={item.heading}
+                    width={1000}
+                    height={1000}
+                    className="object-contain mask-b-from-3.5 group-hover:scale-110 transition-transform delay-150 ease-linear"
+                  />
+                  <div className="absolute inset-0 flex flex-col justify-end px-4 pb-2">
+                    <h2 className="text-sm lg:text-lg font-semibold">
+                      {item.heading}
+                    </h2>
+                    <p className="text-xs lg:text-sm text-white/60">
+                      {item.subheading}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default UseCases
+export default UseCases;
